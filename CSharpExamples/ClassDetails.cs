41c99de630c605427ad7b719780b92c52e7355ec
Jeffry Gonzalez <jeff@hypertheory.com>
2022-02-25 11:22:26 -0500

    started with properties

---
+++ CSharpExamples/ClassDetails.cs
@@ -0,0 +1,29 @@
+﻿
+using CSharpExamples.Examples;
+
+namespace CSharpExamples;
+
+public class ClassDetails
+{
+    [Fact]
+    public void Properties()
+    {
+        var c = new Customer("Pete");
+        //var c2 = new Customer("Laura", 17);
+        var c2 = new Customer("Laura") { 
+            Age = 17 };
+
+        Assert.Equal("Pete", c.Name);
+
+        Assert.Equal(17, c2.Age);
+
+        
+
+
+       // c.Name = "Paul";
+
+
+
+       
+    }
+}
---
+++ CSharpExamples/ClassExamples.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace CSharpExamples.Examples;
+
+
+public class Customer
+{
+    public Customer(string name)
+    {
+        Name = name;
+    }
+
+    // data (state)
+    // fields
+   
+    public int Age { get; init; }
+    // behavior 
+    // Methods, etc. that use that data
+
+    public string Name {  get; private set; } = string.Empty;
+}
